1cc35d4f3806d3b5cdd99668effa20f095716a55
zhoudian64 <1104127978@qq.com>
2019-04-09 08:42:14 +0800

    fix(redis): 添加了环境变量

--- infrastructure/redis.go
+++ infrastructure/redis.go
@@ -1,13 +1,16 @@
 package infrastructure
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"os"
+)
 
 var Redis *redis.Client
 
 func init() {
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
 }
