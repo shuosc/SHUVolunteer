b63687529c45b580bba01418bf2f3b4209165597
longfangsong <longfangsong@icloud.com>
2019-04-19 00:44:50 +0800

    feature(CD): Add CD

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/login", handler.LoginHandler)
 	http.HandleFunc("/ping", handler.TestHandler)
-	http.HandleFunc("/volunteer-activities", handler.VolunteerActivitiesHandler)
+	http.HandleFunc("/activities", handler.VolunteerActivitiesHandler)
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
